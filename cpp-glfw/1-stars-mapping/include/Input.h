e52f0b82b1a293b624832e6dd949be6d0b9a7530
hareldamti <94783416+hareldamti@users.noreply.github.com>
2023-12-06 17:41:33 +0200

    2 can write/read to gl texture, both from cpu and gpu

--- cpp-glfw/1-stars-mapping/include/Input.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "types.h"
-#include "util.h"
-#include "glad/glad.h"
-#include "GLFW/glfw3.h"
-#include "render.h"
-
--- cpp-glfw/1-stars-mapping/shaders/default.frag
+++
@@ -1,6 +0,0 @@
-#version 330 core
-layout (location = 0) in vec2 _uv;
-
-void main() {
-    gl_FragColor = vec4(_uv, 1, 1);
-}
